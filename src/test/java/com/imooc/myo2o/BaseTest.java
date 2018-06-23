6d9014b25fbff859a85e623a7a5af00926be62f7
lgzkd <wodeshijie1234@126.com>
2018-06-23 18:39:35 +0800

    2018/6/23 基本完整的项目

--- src/test/java/com/imooc/myo2o/BaseTest.java
+++ src/test/java/com/imooc/myo2o/BaseTest.java
@@ -9,7 +9,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 // 告诉junit spring配置文件
-@ContextConfiguration({ "classpath:spring/spring-redis.xml","classpath:spring/spring-dao.xml",
+/*@ContextConfiguration({ "classpath:spring/spring-redis.xml","classpath:spring/spring-dao.xml",
+		"classpath:spring/spring-service.xml" })*/
+@ContextConfiguration({ "classpath:spring/spring-dao.xml",
 		"classpath:spring/spring-service.xml" })
 public class BaseTest {
 
